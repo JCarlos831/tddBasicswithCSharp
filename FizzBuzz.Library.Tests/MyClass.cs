f40ac07a31ecc2deff9bcc24fdb2eacb3f1fca96
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-07-26 14:45:25 -0400

    First Commit

---
+++ FizzBuzz.Library.Tests/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void Buzzer_When_1_Retruns1()
+        {
+            // Arrange
+            int input = 1;
+
+            // Act
+            string output = FizzBuzzer.GetValue(input);
+
+            // Assert
+            Assert.AreEqual("1", output);
+        }
+
+        [Test]
+        public void Buzzer_When2_Returns2()
+        {
+            int input = 2;
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("2", output);
+        }
+
+        [Test]
+        public void Buzzer_When3_ReturnsFizz()
+        {
+            int input = 3;
+            string output = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+    }
+}
---
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            if (input == 3)
+                return "Fizz";
+            return input.ToString();
+        }
+    }
+}
